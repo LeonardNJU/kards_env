19824f02ba58958fd39ed0b63ad78d014b5ff4e3
lsamc <2322176165@qq.com>
2025-07-05 12:07:16 +0800

    now add state control to main system and can address the `win` problem. But now there need some fixes.

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 from game.game import Game
 from player.player import Player
+from enum import Enum
+
+class GameState(Enum):
+    INIT = "init"
+    RESCHEDULE = "reschedule"
+    COMBAT = "combat"
+    DONE = "done"
+
+gameState = GameState.INIT
+player1 :Player = None
+player2 :Player = None
+game :Game = None
 
 
-def main():
-    alice = Player("Alice", "simulate/deck1.txt")
-    bob = Player("Bob", "simulate/deck1.txt")
+def init():
+    global player1, player2, game, gameState
+    player1 = Player("Alice", "simulate/deck1.txt")
+    player2 = Player("Bob", "simulate/deck1.txt")
+    game = Game(player1, player2)
+    gameState = GameState.RESCHEDULE
 
-    game = Game(alice, bob)
+def update():
+    pass
 
-    game.start()
+def draw():
+    pass
 
 
 if __name__ == "__main__":
-    main()
+    init()
+    while True:
+        update()
+        draw()
